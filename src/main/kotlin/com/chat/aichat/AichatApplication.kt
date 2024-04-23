d1bdb300c55f0321fea52934eeb2595240721734
Jungwook Kim <jw0905@naver.com>
2024-04-23 23:07:55 +0900

    first commit

---
+++ src/main/kotlin/com/chat/aichat/AichatApplication.kt
@@ -0,0 +1,11 @@
+package com.chat.aichat
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class AichatApplication
+
+fun main(args: Array<String>) {
+	runApplication<AichatApplication>(*args)
+}
---
+++ src/test/kotlin/com/chat/aichat/AichatApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.chat.aichat
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class AichatApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
